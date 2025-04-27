acffd9602da30ebd77bad1d39203dc6f96d1517b
AlexSerh <shunechkiss@gmail.com>
2025-04-27 22:43:35 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,12 +19,10 @@ function searchImages() {
   }
 }
 
-
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabaseUrl = 'https://cptowysiqzrcmhprsgqu.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwdG93eXNpcXpyY21ocHJzZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3Nzg1NDUsImV4cCI6MjA2MTM1NDU0NX0.-RKQIMAIiOl_bqSzGUpMcZebGa89NUZ6xPu9P2p-DbA';
-
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 function addCityToTable(city) {
@@ -75,7 +73,27 @@ document.getElementById('cityForm').addEventListener('submit', async function (e
         alert('Місто додано!');
         addCityToTable(data[0]);
         document.getElementById('cityForm').reset();
+        closeModal();
     }
 });
+
+const openModalButton = document.getElementById('openModalButton');
+const closeModalButton = document.getElementById('closeModalButton');
+const cityModal = document.getElementById('cityModal');
+const modalOverlay = document.getElementById('modalOverlay');
+
+function openModal() {
+    cityModal.style.display = 'block';
+    modalOverlay.style.display = 'block';
+}
+
+function closeModal() {
+    cityModal.style.display = 'none';
+    modalOverlay.style.display = 'none';
+}
+
+openModalButton.addEventListener('click', openModal);
+closeModalButton.addEventListener('click', closeModal);
+modalOverlay.addEventListener('click', closeModal);
 
 loadCities();
